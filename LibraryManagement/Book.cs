b1e6667bb2d297de22c46122b265d22a2c2b3b2b
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-07-12 11:21:32 +0530

    first commit

---
+++ LibraryManagement/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement
+{
+    public class Book
+    {
+        int book_id { get; set; }
+        string title { get; set; }
+        string author { get; set; }
+        string genre { get; set; }
+        string borrowed { get; set; }
+    }
+}
---
+++ LibraryManagement/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace LibraryManagement
+{
+    public class Library
+    {
+       List<Book> ListOfBooks = new List<Book>();
+         public void add_book(book) {
+            
+        
+        }
+         public void get_total_books() { }
+         public void get_available_books() { }
+        public void get_borrowed_books(){ }
+        public void get_books_by_author(author) { }
+
+        public void get_books_by_genre(genre) { }
+        public void borrow_book(book_id, borrower) { }
+        public void return_book(book_id) { }
+        public void get_book_details(book_id) { }
+
+    }
+}
